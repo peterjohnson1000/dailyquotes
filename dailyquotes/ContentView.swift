c68f1f964c40aa10996bfb3a70a0d754dcc241c4
peterjohnson1000 <peterj1298@gmail.com>
2024-03-08 00:10:13 +0530

    Initial Commit

---
+++ dailyquotes/ContentView.swift
@@ -0,0 +1,73 @@
+//
+//  ContentView.swift
+//  dailyquotes
+//
+//  Created by Peter Johnson on 3/6/24.
+//
+
+import SwiftUI
+
+struct QuoteCategory: Codable, Identifiable, Hashable {
+    var id: String = UUID().uuidString
+    let name: String
+    let allQuotes: [Quote]
+}
+
+struct Quote: Codable, Hashable, Identifiable {
+    var id: String = UUID().uuidString
+    var quote: String
+}
+
+
+struct ContentView: View {
+    
+    @State var categories: [QuoteCategory] = []
+    
+    func loadQuotes() {
+        if let url = Bundle.main.url(forResource: "quotes", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                self.categories = try JSONDecoder().decode([QuoteCategory].self, from: data)
+                print(self.categories)
+            } catch {
+                print("Error decoding JSON: \(error)")
+            }
+        }
+    }
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(categories) { data in
+                    NavigationLink(value: data)
+                    {
+                        Text(data.name)
+                    }
+                }
+            }
+            .navigationTitle("Quotes Category")
+            .navigationDestination(for: QuoteCategory.self) {data in
+                MotivationalView(quotes: data)
+            }
+        }
+        .onAppear {
+            loadQuotes()
+        }
+    }
+}
+
+struct MotivationalView: View {
+    let quotes: QuoteCategory
+    var body: some View {
+        List {
+            ForEach(quotes.allQuotes) {eachQuote in
+                Text(eachQuote.quote)
+                    .lineLimit(1)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ dailyquotes/dailyquotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  dailyquotesApp.swift
+//  dailyquotes
+//
+//  Created by Peter Johnson on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct dailyquotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
